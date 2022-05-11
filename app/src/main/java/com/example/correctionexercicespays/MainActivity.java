8698514b2cf815e5a03bb8b4e0e3c5cfea395458
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-11 17:37:55 +0100

    Remplir spinner

--- app/src/main/java/com/example/correctionexercicespays/MainActivity.java
+++ app/src/main/java/com/example/correctionexercicespays/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.correctionexercicespays;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Pays> pays = new ArrayList<>();
+    Spinner sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sp = findViewById(R.id.sp);
 
         pays.add(new Pays("Maroc","Rabat","Afric",676767,R.drawable.ma));
         pays.add(new Pays("Egypt","Cairo","Afric",767676,R.drawable.eg));
@@ -23,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
         pays.add(new Pays("Japan","Tokyo","Asie",32323,R.drawable.jp));
         pays.add(new Pays("France","Paris","Europe",232323,R.drawable.fr));
         pays.add(new Pays("Portugal","Lesbon","Europe",23232,R.drawable.pt));
+
+
+        ArrayList<String> nomsPays = new ArrayList<>();
+        for(Pays p : pays)
+            nomsPays.add(p.getNom());
+
+        ArrayAdapter<String> ad = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,nomsPays);
+        sp.setAdapter(ad);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
