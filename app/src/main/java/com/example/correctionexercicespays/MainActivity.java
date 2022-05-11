cd2359b33212d021fbeffc86f58533dfe5c600d0
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-11 17:28:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/correctionexercicespays/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.correctionexercicespays;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Pays> pays = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        pays.add(new Pays("Maroc","Rabat","Afric",676767,R.drawable.ma));
+        pays.add(new Pays("Egypt","Cairo","Afric",767676,R.drawable.eg));
+        pays.add(new Pays("Espagne","Madrid","Europe",3434343,R.drawable.es));
+        pays.add(new Pays("Argentine","Buenos Aires","Amerique",3444343,R.drawable.ar));
+        pays.add(new Pays("Belgique","Bruxelle","Europe",232323,R.drawable.be));
+        pays.add(new Pays("Japan","Tokyo","Asie",32323,R.drawable.jp));
+        pays.add(new Pays("France","Paris","Europe",232323,R.drawable.fr));
+        pays.add(new Pays("Portugal","Lesbon","Europe",23232,R.drawable.pt));
+    }
+}
